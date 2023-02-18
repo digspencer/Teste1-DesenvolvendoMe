96f104d42e72de0c726af42a95f5e5aa5c2d4ace
digspencer <diegoiglan@hotmail.com>
2023-02-18 08:47:43 -0300

    Teste1.js

--- Teste1.js
+++ Teste1.js
@@ -2,29 +2,16 @@
 //do contrário ela deve retornar falso. (palíndromos são palavras ou frases que podem ser
 // lidas da esquerda para a direita ou da direita para a esquerda.)
 
-let palavras = ["asa", "ovo", "banana", "pão", "amor", "carro", "ababa"];
+const ehPalindromo = (text) => {
+    const palindromo = text.split('')
+    .reverse()
+    .join('');
 
-function SePalindromo(palavras) {
-    //Verifica Letra por Letra
-    var totalDeLetras = palavras.length;
-    var letrasAconferir = Math.floor (totalDeLetras / 2);
-    for (var i = 0; i < letrasAconferir; i++){
-        var letras = palavras[i];
-        var indice = i;
-        var compararIndice = totalDeLetras - 1 - indice;
-        var compararLetras = palavras[compararIndice];
-        if (letras =! compararLetras){
-            return false;
-        }
+    if (palindromo === text) {
+        console.log("A palavra " + text + " é Palíndromo");
+    } else {
+        console.log("A palavra " + text + " não é Palíndromo");
     }
-
-    return true;
 }
 
-    palavras.forEach((palavras, ind) => {
-        if (SePalindromo(palavras)) {
-            console.log("A Palavra", palavras, "é Palíndrono");
-        } else {
-            console.log("A Palavra", palavras, "não é Palíndrono");
-        }
-    })
+ehPalindromo("asa");

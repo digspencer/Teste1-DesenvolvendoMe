d078791c2cb63ed26e3a4d5429fc45c0993d7f17
DiegoPaulino <diegoiglan@hotmail.com>
2023-02-20 14:05:43 -0300

    Teste1.js

--- Teste1.js
+++ Teste1.js
@@ -1,17 +1,33 @@
-// 1) Faça uma função que receba uma string e retorne verdadeiro caso ela seja palíndromo,
-//do contrário ela deve retornar falso. (palíndromos são palavras ou frases que podem ser
-// lidas da esquerda para a direita ou da direita para a esquerda.)
+/* TESTE 1 Faça uma função que receba uma string e retorne verdadeiro caso ela seja palíndromo,
+do contrário ela deve retornar falso. (palíndromos são palavras ou frases que podem ser
+lidas da esquerda para a direita ou da direita para a esquerda.) */
 
-const ehPalindromo = (text) => {
-    const palindromo = text.split('')
-    .reverse()
-    .join('');
+function palindromo(text){
+    let sePalindormo = text.split('')
+        .reverse()
+        .join('');
 
-    if (palindromo === text) {
-        console.log("A palavra " + text + " é Palíndromo");
+        if ( sePalindormo === text){
+            console.log(text + " é Palindromo");
+        } else {
+            console.log(text + " não é Palindromo");
+        }
+}
+
+palindromo("asa");
+
+/* TESTE 1.2 Faça uma função que receba uma string contendo um CPF e verifique se ele está
+formatado de acordo com a máscara 999.999.999-99. A função deve usar regex para fazer
+a validação da máscara e deve retornar verdadeiro ou falso.
+cpf = “461.636.517-23” */
+
+function validarCPF(cpf) {
+    const validar = cpf.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    if (cpf === validar){
+        console.log("A máscara está correta")
     } else {
-        console.log("A palavra " + text + " não é Palíndromo");
+    console.log("A máscara está incorreta")
     }
 }
 
-ehPalindromo("asa");
+validarCPF("034.442.095-76");
